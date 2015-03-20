7529080086ef75a458f22e98e5818c00e598b04f
Steven Wilkin <stevenwilkin@gmail.com>
2015-03-20 15:33:05 +0000

    Add script to process CAS csv

---
+++ bin/geocode_cas.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+require 'geocoder'
+require 'json'
+
+locations = []
+
+# "Aberdeen Citizens Advice Bureau",,"41 Union Street","Aberdeen","AB11 5BN","01224 569 750","Not available","298,721",910,"a","a"
+CSV.foreach('cas.csv', headers: true, return_headers: false) do |row|
+  title = row.fields.first.strip
+  address = row.fields[1..4].reject { |value| value.nil? || value.strip == '' }.map(&:strip).join(', ')
+  phone = row.fields[5]
+  lat, lng = Geocoder.coordinates(address)
+
+  locations << {
+    title: title,
+    address: address,
+    phone: phone,
+    lat: lat,
+    lng: lng
+  }
+  #sleep(1)
+end
+
+puts JSON.pretty_generate(locations)
