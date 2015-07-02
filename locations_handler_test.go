97ca625eeda02b6ea1b57a75bcc89c7719b142e5
Steven Wilkin <stevenwilkin@gmail.com>
2015-07-02 16:42:16 +0100

    Test booking_location_id corresponds to a location id

--- locations_handler_test.go
+++ locations_handler_test.go
@@ -102,3 +102,26 @@ func TestLocationIdsAreUnique(t *testing.T) {
 		}
 	}
 }
+
+func TestBookingLocationIdsAreValidLocations(t *testing.T) {
+	locations := loadLocations()
+
+	for _, location := range locations {
+		if location.BookingLocationId == "" {
+			continue;
+		}
+
+		bookingLocationFound := false;
+
+		for _, otherLocation := range locations {
+			if location.BookingLocationId == otherLocation.Id {
+				bookingLocationFound = true;
+				break;
+			}
+		}
+
+		if !bookingLocationFound {
+			t.Error("Booking location not found:", location.BookingLocationId)
+		}
+	}
+}
